c1a5044e110099ff5339ba1dde90557431b64467
Trev <greenwoodtrevon@gmail.com>
2021-05-27 16:59:20 -0600

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueConteinerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,10 +17,26 @@ var getRepoIssues = function(repo) {
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html")
     }
   });
 };
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  
+  if(repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+}
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -70,4 +87,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
